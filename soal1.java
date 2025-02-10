6f5ac2cab017a319298164e4914bde2f77ce1a58
vermilion10 <180294769+vermilion10@users.noreply.github.com>
2025-02-10 13:57:05 +0700

    ver1.3

--- soal1.java
+++ soal1.java
@@ -1,8 +1,4 @@
-/**
- * This program displays penjumlahan angka
- * @version 1.0 2025-02-10
- * @author Fathin Yassarahman 
- */
+
 public class soal1 {
     public static void main(String[] args){
         byte angka1 = 125;
--- soal2.java
+++ soal2.java
@@ -1,8 +1,3 @@
-/**
- * This program display string sesuai kondisi
- * @version 1.0 2025-02-10
- * @author Fathin Yassarahman 
- */
 public class soal2 {
     public static void main(String[] args){
         int i = 42;
